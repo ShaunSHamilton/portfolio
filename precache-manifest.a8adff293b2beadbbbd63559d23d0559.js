4015338e9f9ca03d9abc5ca531dab5b3a0140d7d
Sky020 <2397860h@student.gla.ac.uk>
2020-04-29 16:13:17 +0100

    Updates

--- precache-manifest.a8adff293b2beadbbbd63559d23d0559.js
+++ precache-manifest.a8adff293b2beadbbbd63559d23d0559.js
@@ -1,31 +1,35 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "70157951828f4da7eee8db5b79e9ca13",
+    "revision": "cdeb10069c258d95e60f4246b98b066e",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "1a0340e35ebee96c421a",
+    "revision": "7dd5400726de7eb15305",
     "url": "/portfolio/static/css/2.74189dbb.chunk.css"
   },
   {
-    "revision": "f909fab49ef48257cc16",
-    "url": "/portfolio/static/css/main.aa6fe849.chunk.css"
+    "revision": "c8d3f49ee2f1f714507d",
+    "url": "/portfolio/static/css/main.a056729f.chunk.css"
   },
   {
-    "revision": "1a0340e35ebee96c421a",
-    "url": "/portfolio/static/js/2.04bc796a.chunk.js"
+    "revision": "7dd5400726de7eb15305",
+    "url": "/portfolio/static/js/2.0c25f3f6.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/portfolio/static/js/2.04bc796a.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.0c25f3f6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f909fab49ef48257cc16",
-    "url": "/portfolio/static/js/main.6a4b236b.chunk.js"
+    "revision": "c8d3f49ee2f1f714507d",
+    "url": "/portfolio/static/js/main.6e0b33f5.chunk.js"
   },
   {
     "revision": "6c1d3ad26a766dc494c1",
     "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "086404dd74c96fbd44ab1a9531cc9460",
+    "url": "/portfolio/static/media/MarkDown.086404dd.png"
   },
   {
     "revision": "19e88e56fa2fad4e156a4c1d76f6419d",
@@ -34,6 +38,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "c786999338609d5cc40dc53b70b3cc7f",
     "url": "/portfolio/static/media/RandPoundApp.c7869993.png"
+  },
+  {
+    "revision": "32f5f353a2cbf0b08349433cbe5d21ca",
+    "url": "/portfolio/static/media/RandomQuoteMachine.32f5f353.png"
   },
   {
     "revision": "002d4199ecaa426a2651f294efd6a971",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f134c54e0e15472fe80ccb4e7dcb5560.js"
+  "/portfolio/precache-manifest.a8adff293b2beadbbbd63559d23d0559.js"
 );
 
 self.addEventListener('message', (event) => {
