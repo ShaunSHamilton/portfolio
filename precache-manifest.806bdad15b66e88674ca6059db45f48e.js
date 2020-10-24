479548958e0a32310c554b17d3d557aedddf4806
Sky020 <2397860h@student.gla.ac.uk>
2020-10-24 21:38:22 +0100

    Updates

---
+++ precache-manifest.806bdad15b66e88674ca6059db45f48e.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41c7f108eece4c11e2a5556f38e6d23a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "882e9cb8322c43b56813",
+    "url": "/portfolio/static/css/2.b5b4534f.chunk.css"
+  },
+  {
+    "revision": "b0212d0916b0b452c30b",
+    "url": "/portfolio/static/css/main.c3841a2f.chunk.css"
+  },
+  {
+    "revision": "882e9cb8322c43b56813",
+    "url": "/portfolio/static/js/2.32c132fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.32c132fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0212d0916b0b452c30b",
+    "url": "/portfolio/static/js/main.55ace56c.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "f42b6d1de495a0571d09a5cd542bc0fd",
+    "url": "/portfolio/static/media/EnergyTracker.f42b6d1d.png"
+  },
+  {
+    "revision": "e324d69374b69694dbeaeccaec2ad6e2",
+    "url": "/portfolio/static/media/EnergyTracker.png-large.e324d693.jpg"
+  },
+  {
+    "revision": "85e64cc46633292ab05041d0d01e716a",
+    "url": "/portfolio/static/media/GardenPlanner.85e64cc4.png"
+  },
+  {
+    "revision": "edf6e64a0335882691271073edb60b84",
+    "url": "/portfolio/static/media/GardenPlanner.png-large.edf6e64a.jpg"
+  },
+  {
+    "revision": "a9553e950bb8e6bbd2d2ded0802773ff",
+    "url": "/portfolio/static/media/GardenPlanner.png-medium.a9553e95.jpg"
+  },
+  {
+    "revision": "edcc66b4306845ac8f7f3b9cd646c2f0",
+    "url": "/portfolio/static/media/JavaScriptCalc.edcc66b4.png"
+  },
+  {
+    "revision": "b7798de5f55e9c4a3dd61a8eae74ceac",
+    "url": "/portfolio/static/media/JavaScriptCalc.png-large.b7798de5.jpg"
+  },
+  {
+    "revision": "dec5c36c26836e8d4eaced20ee8a9c91",
+    "url": "/portfolio/static/media/JavaScriptCalc.png-medium.dec5c36c.jpg"
+  },
+  {
+    "revision": "086404dd74c96fbd44ab1a9531cc9460",
+    "url": "/portfolio/static/media/MarkDown.086404dd.png"
+  },
+  {
+    "revision": "34c83e7d4ccf8c946e2b2b22e9b5c01b",
+    "url": "/portfolio/static/media/MarkDown.png-large.34c83e7d.jpg"
+  },
+  {
+    "revision": "5cc43545dcfdae7acea943da877f2300",
+    "url": "/portfolio/static/media/MarkDown.png-medium.5cc43545.jpg"
+  },
+  {
+    "revision": "19e88e56fa2fad4e156a4c1d76f6419d",
+    "url": "/portfolio/static/media/NetskipCode.19e88e56.png"
+  },
+  {
+    "revision": "1321f58ec7fb6be833cacfd78e857e61",
+    "url": "/portfolio/static/media/NetskipCode.png-large.1321f58e.jpg"
+  },
+  {
+    "revision": "4a6d6dc105e2bd18416f395c6c8e1b0e",
+    "url": "/portfolio/static/media/NetskipCode.png-medium.4a6d6dc1.jpg"
+  },
+  {
+    "revision": "d06b361c420ed708849632637d7e9860",
+    "url": "/portfolio/static/media/PotterPlayer.d06b361c.png"
+  },
+  {
+    "revision": "59f18f3b0cee582511e0570bf47b1c16",
+    "url": "/portfolio/static/media/PotterPlayer.png-large.59f18f3b.jpg"
+  },
+  {
+    "revision": "1a7025d51ca90f9288b637f57b660826",
+    "url": "/portfolio/static/media/PotterPlayer.png-medium.1a7025d5.jpg"
+  },
+  {
+    "revision": "c786999338609d5cc40dc53b70b3cc7f",
+    "url": "/portfolio/static/media/RandPoundApp.c7869993.png"
+  },
+  {
+    "revision": "3fa2036f352edc43bc227cbf654e3254",
+    "url": "/portfolio/static/media/RandPoundApp.png-large.3fa2036f.jpg"
+  },
+  {
+    "revision": "32f5f353a2cbf0b08349433cbe5d21ca",
+    "url": "/portfolio/static/media/RandomQuoteMachine.32f5f353.png"
+  },
+  {
+    "revision": "4b707b6678debed71d37dd3af032059f",
+    "url": "/portfolio/static/media/RandomQuoteMachine.png-large.4b707b66.jpg"
+  },
+  {
+    "revision": "002d4199ecaa426a2651f294efd6a971",
+    "url": "/portfolio/static/media/UltimateGuitarCode.002d4199.png"
+  },
+  {
+    "revision": "6861ed1f65e30fc3e77e439fce58b7ef",
+    "url": "/portfolio/static/media/UltimateGuitarCode.png-large.6861ed1f.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.650397c2dfec18db29d878b584dd49c3.js"
+  "/portfolio/precache-manifest.806bdad15b66e88674ca6059db45f48e.js"
 );
 
 self.addEventListener('message', (event) => {
